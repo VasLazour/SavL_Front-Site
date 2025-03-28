90dee4168507f0b62f30c20cf5897504e28a02f7
VasLazour <108477818+VasLazour@users.noreply.github.com>
2025-03-28 15:02:50 +0200

    Update myapp_Video.js

--- myapp_Video.js
+++ myapp_Video.js
@@ -1,6 +1,6 @@
 let listVideo = document.querySelectorAll('.video-list .vid');
 let mainVideo = document.querySelector( '.main-video video');
-let title = document.querySelectorAll( '.main-video .title');
+let title = document.querySelector( '.main-video .title');
 
 listVideo.forEach (video => {
   video.onclick = () => {
